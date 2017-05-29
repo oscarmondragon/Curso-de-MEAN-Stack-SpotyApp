bc7a9c4b919ab217cc2cc92d08ffb424f3a197ee
Oscar <oscarmondragon100@gmail.com>
2017-05-29 13:36:22 -0500

    Se reviso la app pero no hubo cambios

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -16,7 +16,6 @@ exports.ensureAuth = function(req,res,next){
   } catch(ex){
     console.log(ex);
     return res.status(404).send({message: "Token no valido"});
-
   }
 
   req.user = payload;
--- routes/user-route.js
+++ routes/user-route.js
@@ -3,8 +3,10 @@ var express = require('express');
 var UserController = require('../controllers/user-controller');
 var api = express.Router();
 var md_auth= require('../middlewares/authentication');
+
 api.get('/probando-controlador',md_auth.ensureAuth, UserController.pruebas);
 module.exports = api;
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/update-user/:id',md_auth.ensureAuth, UserController.updateUser);
 module.exports = api;
